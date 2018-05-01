369ce3c0de2652e69c79bdd33e57427f0d681a09
Akihiro Nishiya <akihiro.nishiya@gmail.com>
2018-05-01 03:09:12 +0000

    add react

--- src/panel-main.js
+++ src/panel-main.js
@@ -1,21 +1,39 @@
-const testEval = () => {
-  const expr = 'console.log("test eval", getEventListeners)';
-  chrome.devtools.inspectedWindow.eval(expr, (isLoaded, isException) => {
-    if (isException) {
-      throw new Error('Eval failed for ' + expr);
-    }
-    console.log('eval success!', isLoaded);
-  });
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const reloadPage = () => {
-  const options = {
-    ignoreCache: true
-  };
-  chrome.devtools.inspectedWindow.reload(options);
-};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleGetListeners = this.handleGetListeners.bind(this);
+  }
 
-window.addEventListener('load', () => {
-  var reloadButton = document.querySelector('.reload-button');
-  reloadButton.addEventListener('click', reloadPage);
-});
+  handleGetListeners() {
+    const expr = 'console.log("test eval", getEventListeners)';
+    chrome.devtools.inspectedWindow.eval(expr, (isLoaded, isException) => {
+      if (isException) {
+        throw new Error('Eval failed for ' + expr);
+      }
+      console.log('eval success!', isLoaded);
+    });
+  }
+
+  handleReload() {
+    const options = {
+      ignoreCache: true
+    };
+    chrome.devtools.inspectedWindow.reload(options);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <button class='button' onClick={this.handleGetListeners}>Get Listeners</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
